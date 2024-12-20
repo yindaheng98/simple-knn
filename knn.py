5735b33bd249b9ed5495b23d058cf9807e0d9a64
yindaheng98 <yindaheng98@gmail.com>
2024-12-19 21:43:07 -0800

    all output distance

--- knn.py
+++ knn.py
@@ -3,10 +3,10 @@ from simple_knn import knn_batch, knn_kernel
 
 points = torch.rand(10000, 3).cuda()
 K = 3
-dists = ((points.unsqueeze(0) - points.unsqueeze(1))**2).sum(-1)
+dists = ((points.unsqueeze(0) - points.unsqueeze(1))**2).sum(-1).sqrt()
 best, indices = dists.topk(K + 1, dim=1, largest=False)
 best, indices = best[:, 1:], indices[:, 1:]
 indices_kernel, best_kernel = knn_kernel(points, K)
 print((best - best_kernel).abs().max(), (indices - indices_kernel).abs().max())
 indices_batch, best_batch = knn_batch(points, K, batch=8)
-print((best - best_batch**2).abs().max(), (indices - indices_batch).abs().max())
+print((best - best_batch).abs().max(), (indices - indices_batch).abs().max())
--- simple_knn/kernel.py
+++ simple_knn/kernel.py
@@ -7,4 +7,4 @@ from ._C import KNN
 def knn(points: torch.Tensor, k=8):
     """Get k neighbor points for each point."""
     best, indices = KNN(points, k)
-    return indices, best
+    return indices, best.sqrt()
